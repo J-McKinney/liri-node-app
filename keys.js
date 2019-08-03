258cd35800fa79d32cdf8410c44759ba340eb329
Jesse McKinney <jessman51386@gmail.com>
2019-08-03 09:24:33 -0400

    added some variables

--- keys.js
+++ keys.js
@@ -1,1 +1,7 @@
-You need to fill this in
+// console.log("this is loaded");
+
+// exports.spotify = {
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+// };
+
--- liri.js
+++ liri.js
@@ -1,1 +1,5 @@
-You need to fill this in
+//Ah ah ah, you didn't say the magic word...
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var spotify = new spotify(keys.spotify);
