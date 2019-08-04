3aa7adc9362ce4db8e41ed5bdf6be1991b09cbd7
Jesse McKinney <jessman51386@gmail.com>
2019-08-04 14:41:49 -0400

    added vars and switch case function, added keys and a log txt file

--- liri.js
+++ liri.js
@@ -1,5 +1,33 @@
 //Ah ah ah, you didn't say the magic word...
 require("dotenv").config();
 
+//variables
 var keys = require("./keys.js");
 var spotify = new spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var fs = require("fs");
+var userOption = process.argv[2];
+var userParameter = process.argv[3];
+
+userInput(userOption, userParameter);
+
+//Functions
+function userInput(userOption, userParameters) {
+    switch (userOption) {
+        case "concert-this":
+            concertInfo(userParameter);
+            break;
+        case "spotify-this-song":
+            songInfo(userParameter);
+            break;
+        case "movie-this":
+            movieInfo(userParameter);
+            break;
+        case "do-what-it-says":
+            someInfo();
+            break;
+        default:
+            console.log("Ah ah ah, you didn't say the magic word...")
+    }
+}
