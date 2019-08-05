3e44d4beb3c4ac192da70c1612bde747a96761fa
Jesse McKinney <jessman51386@gmail.com>
2019-08-05 16:13:49 -0400

    I got omdb working but for only one word titles, need to figure out whats up with spotify too???

--- liri.js
+++ liri.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 
 //variables
 var keys = require("./keys.js");
-var spotify = new spotify(keys.spotify);
-var Spotify = require("node-spotify-api");
+// var spotify = new spotify(keys.spotify);
+// var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var fs = require("fs");
 var userOption = process.argv[2];
 var userParameter = process.argv[3];
+var moment = require("moment");
+moment().format();
 
 userInput(userOption, userParameter);
 
@@ -31,3 +33,26 @@ function userInput(userOption, userParameters) {
             console.log("Ah ah ah, you didn't say the magic word...")
     }
 }
+
+function movieInfo(userParameter) {
+    if (!userParameter) {
+        userParameter = "Mr.Nobody";
+    }
+    axios.get("http://www.omdbapi.com/?t=" + userParameter + "&y=&plot=short&apikey=trilogy").then(function (response) {
+        var movieResults =
+            "======================" +
+            "\nMovie Title: " + response.data.Title +
+            "\nYear of Release: " + response.data.Year +
+            "\nIMDB Rating: " + response.data.imdbRating +
+            "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+            "\nCountry Produced: " + response.data.Country +
+            "\nLanguage: " + response.data.Language +
+            "\nPlot: " + response.data.Plot +
+            "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+};
+
